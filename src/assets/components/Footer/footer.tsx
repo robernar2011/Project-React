f5a2b5888aa6e90abe1a533efc5c33c472f22f09
Azevedo <karolina.azevedo@sap.com>
2024-02-21 20:34:43 -0300

    inicio-react-rotas-pages

---
+++ src/assets/components/Footer/footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div className="footer">
+             <p className="slogan">"Ninguém educa ninguém, ninguém educa a si mesmo, as pessoas se educam entre si, mediadas
+                pelo mundo." - Paulo Freire </p>
+            <p>&copy; 2023 | Site desenvolvido por G4</p>
+        </div>
+    )
+}
+
+export default Footer
